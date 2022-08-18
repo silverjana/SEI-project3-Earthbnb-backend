b4985ff6781200301e2841876e6187188efa7145
az85 <aaron.zahl@gmail.com>
2022-08-18 10:47:50 +0100

    adding some starter code to server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = express()
+
+const startServer = async () => {
+  await connectToDb()
+  console.log("Database has connected successfully");
+
+  app.listen(CONSTS.PORT, () => {
+    console.log(`🚀 Express server running on port ${CONSTS.PORT} 🚀`);
+  })
+}
+
+startServer()
