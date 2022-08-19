205d4e10b0ac7cfb6bfe941530d3cba13a221fea
Arianna Giordano <silverjana@live.com>
2022-08-19 18:38:34 +0100

    deploy 02 app prepared

--- server.js
+++ server.js
@@ -4,28 +4,36 @@ import CONSTS from './consts.js'
 import cors from "cors"
 import logger from './middleware/logger.js'
 import router from "./router.js"
+import errorHandler from './middleware/errorHandler.js'
+import dotenv from "dotenv"
 
-const app = express()
+// ! deployment - Move my code into a function.
+async function startServer() {
 
-app.use(cors())
+  const app = express()
+  dotenv.config()
+  app.use(cors())
 
-app.use(express.json())
+  app.use(express.json())
 
-app.use(logger)
+  app.use(logger)
 
-app.use(router)
+  app.use(router)
 
-app.use((req, res, next) => {
-  return res.status(404).send("404 - Required endpoint not found.");
-})
+  app.use(errorHandler)
 
-const startServer = async () => {
+  // make sure the connectToDb function
+  // actually uses the connection string for the cloud database from the env variable
   await connectToDb()
-  console.log("Database has connected successfully");
 
-  app.listen(CONSTS.PORT, () => {
-    console.log(`🚀 Express server running on port ${CONSTS.PORT} 🚀`);
-  })
+  // app.use((req, res, next) => {
+  //   return res.status(404).send("404 - Required endpoint not found.");
+  // })
+
+
+  app.listen(CONSTS.PORT, () =>
+    console.log(`🚀 Express server running on port ${CONSTS.PORT} 🚀`)
+  )
 }
 
 startServer()
