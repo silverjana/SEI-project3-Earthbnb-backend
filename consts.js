23b2dfab6b62f71cc94b9bf8fc1c80a4ad0626d3
Arianna Giordano <silverjana@live.com>
2022-08-21 13:24:24 +0100

    cleaned consts

--- consts.js
+++ consts.js
@@ -3,11 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const consts = {
-<<<<<<< HEAD
-  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/allproperties",
-=======
   DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
->>>>>>> a0ba71e5128842d622dc09e883ddf1f620ac8eea
   PORT: process.env.PORT || 4000,
   JWT_SECRET: process.env.JWT_SECRET || "JWT_SECRET",
 }
