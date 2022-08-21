28e6fc3280d9fd2e9d2d78b1547adc24fad85ecf
Arianna Giordano <silverjana@live.com>
2022-08-21 13:09:32 +0100

    nothing

--- consts.js
+++ consts.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const consts = {
-  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/project3" , // not sure about this string
-  PORT: process.env.PORT || 3000,
+  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/project3", // not sure about this string
+  PORT: process.env.PORT || 4000,
   JWT_SECRET: process.env.JWT_SECRET || "JWT_SECRET",
 }
 
